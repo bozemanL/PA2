77e509fe3c2e932b15584e1c8781d40df039e265
Lillian Bozeman <lillianbozeman@gmail.com>
2025-10-31 16:56:44 -0700

    Implement downheap.

--- heap.h
+++ heap.h
@@ -54,7 +54,29 @@ struct MinHeap {
     }
 
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+        while (true) {
+            int left = 2 * pos + 1;
+            int right = 2 * pos + 2;
+            int smallest = pos;
+
+            if (left < size && weightArr[data[left]] < weightArr[data[smallest]]) {
+                smallest = left;
+            }
+
+            if (right < size && weightArr[data[right]] < weightArr[data[smallest]]) {
+                smallest = right;
+            }
+
+            if (smallest == pos) {
+                break;
+            }
+
+            int temp = data[pos];
+            data[pos] = data[smallest];
+            data[smallest] = temp;
+
+            pos = smallest;
+        }
     }
 };
 
